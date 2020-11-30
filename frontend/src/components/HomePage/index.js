830d7a6aa23bb107dd79922534f11b9b63c36f65
dale.sakamoto <daletsakamoto@gmail.com>
2020-11-29 23:49:24 -0800

    ugh

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import {NavLink, Redirect} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 import './HomePage.css';
 import HomeLogoAlternate from './HomeLogoAlternate'
